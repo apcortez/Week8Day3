7830954c1c84be045fbe818fb6aa0ef7365e03c1
Angelica Cortez <angelica.cortez@hotmail.it>
2021-09-16 11:27:49 +0200

    add checkout

--- Pizzeria/Script.js
+++ Pizzeria/Script.js
@@ -1,5 +1,25 @@
-﻿var slideIndex = 1;
-showSlides(slideIndex);
+﻿function checkoutForm() {
+    var margherita = parseInt(document.getElementById("margherita").value);
+    var stagioni = parseInt(document.getElementById("4stagioni").value);
+    var diavola = parseInt(document.getElementById("diavola").value);
+    var bufalina = parseInt(document.getElementById("bufalina").value);
+    var web = parseInt(document.getElementById("web").value);
+    var totalepizze = margherita + stagioni + diavola + bufalina + web;
+    if (totalepizze != 0) {
+        localStorage.setItem('TotaleOrdine', totalepizze);
+    }
+    else {
+        alert("Non hai selezionato nessuna pizza");
+    }
+}
+
+function getTotale() {
+    let storedTotale = localStorage.getItem('TotaleOrdine');
+    if (storedTotale != null) {
+        alert("Il totale da pagare è " + storedTotale + " Euro.\nInserisci le informazioni richieste per procedere all'ordinazione.");
+
+    }
+}
 
 // Next/previous controls
 function plusSlides(n) {
